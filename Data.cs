5a4497a77125aef8f1424b75c19bb97f393beb3d
karim <appdomain.karim@gmail.com>
2020-03-25 16:55:50 +0430

    initial commit

---
+++ Data.cs
@@ -0,0 +1,68 @@
+﻿namespace SendOrder
+{
+    using Newtonsoft.Json;
+    using System.IO;
+
+    public class Data
+    {
+        private static Data instance;
+
+        public static Data Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = ReadData();
+                }
+
+                return instance;
+            }
+        }
+
+        public static Data ReadData()
+        {
+            return JsonConvert.DeserializeObject<Data>(File.ReadAllText("Data.json"));
+        }
+
+        public Proxy Proxy { get; set; }
+        public Order Order { get; set; }
+        public string Cookie { get; set; }
+        public string StartsAt { get; set; }
+        public string EndsAt { get; set; }
+        public int SendCount { get; set; }
+        public int SendInterval { get; set; }
+        public int NonceInterval { get; set; }
+    }
+
+    public class Proxy
+    {
+        public bool Enabled { get; set; }
+        public string Value { get; set; }
+    }
+
+    public class Order
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public string CustomerTitle { get; set; }
+        public string OrderSide { get; set; }
+        public int OrderSideId { get; set; }
+        public int Price { get; set; }
+        public int Quantity { get; set; }
+        public int Value { get; set; }
+        public string ValidityDate { get; set; }
+        public string MinimumQuantity { get; set; }
+        public string DisclosedQuantity { get; set; }
+        public int ValidityType { get; set; }
+        public int InstrumentId { get; set; }
+        public string InstrumentIsin { get; set; }
+        public string InstrumentName { get; set; }
+        public int BankAccountId { get; set; }
+        public int ExpectedRemainingQuantity { get; set; }
+        public int TradedQuantity { get; set; }
+        public string CategoryId { get; set; }
+        public int RemainingQuantity { get; set; }
+        public int OrderExecuterId { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,196 @@
+﻿namespace SendOrder
+{
+    using Newtonsoft.Json;
+    using System;
+    using System.Diagnostics;
+    using System.Globalization;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Http;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    internal static class Program
+    {
+        private static StringContent orderContent = null;
+
+        internal static void Main(string[] args)
+        {
+            Log($"Start running app");
+
+            var nonceUrl = $"{"https:"}//online.agah.com/Order/GenerateNonce";
+            var sendOrderUrl = $"{"https:"}//online.agah.com/Order/SendOrder";
+            using var httpClient = GetHttpClient();
+            var emptyContent = new StringContent(string.Empty, Encoding.UTF8, "application/json");
+
+            // Testing connection
+            Log("Testing connection...");
+            var testStopwatch = Stopwatch.StartNew();
+            try
+            {
+                using var nonceResponse = httpClient.PostAsync(nonceUrl, emptyContent).Result;
+                if (nonceResponse.StatusCode != HttpStatusCode.OK)
+                {
+                    throw new Exception(nonceResponse.StatusCode.ToString());
+                }
+
+                nonceResponse.Content.ReadAsStringAsync().Wait();
+            }
+            catch (Exception ex)
+            {
+                LogError("Error: " + ex.Message);
+                Console.ReadKey();
+                return;
+            }
+            finally
+            {
+                testStopwatch.Stop();
+            }
+
+            Log($"Connection is OK. A nonce was taken in {testStopwatch.Elapsed.TotalSeconds} seconds");
+
+            TimeSpan startsAt = TimeSpan.ParseExact(Data.Instance.StartsAt, @"h\:m\:s\:fff", CultureInfo.InvariantCulture);
+            TimeSpan endsAt = TimeSpan.ParseExact(Data.Instance.EndsAt, @"h\:m\:s\:fff", CultureInfo.InvariantCulture);
+            DateTime startDateTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, startsAt.Hours, startsAt.Minutes, startsAt.Seconds, startsAt.Milliseconds);
+            DateTime endDateTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, endsAt.Hours, endsAt.Minutes, endsAt.Seconds, endsAt.Milliseconds);
+
+            void GetNonceAndSetOrderContent()
+            {
+                Log($"Getting nonce ...");
+                using var nonceResponse = httpClient.PostAsync(nonceUrl, emptyContent).Result;
+                if (nonceResponse.StatusCode != HttpStatusCode.OK)
+                {
+                    throw new Exception("Error while getting nonce: " + nonceResponse.StatusCode.ToString());
+                }
+
+                string nonceValue = nonceResponse.Content.ReadAsStringAsync().Result;
+                if (nonceValue.StartsWith("\"") && nonceValue.EndsWith("\""))
+                {
+                    nonceValue = nonceValue.Substring(1, nonceValue.Length - 2);
+                }
+
+                Log("Nonce is: " + nonceValue);
+                string orderPayload = JsonConvert.SerializeObject(new { orderModel = Data.Instance.Order, nonce = nonceValue });
+                orderContent = new StringContent(orderPayload, Encoding.UTF8, "application/json");
+            }
+
+            Thread nonceThread = new Thread(new ThreadStart(() =>
+            {
+                do
+                {
+                    Thread.Sleep(Data.Instance.NonceInterval);
+                    if (DateTime.Now >= endDateTime)
+                    {
+                        break;
+                    }
+
+                    try
+                    {
+                        GetNonceAndSetOrderContent();
+                    }
+                    catch (Exception ex)
+                    {
+                        LogError("Error: " + ex.Message);
+                    }
+                }
+                while (true);
+            }));
+            Thread orderThread = new Thread(new ThreadStart(() =>
+            {
+                do
+                {
+                    if (orderContent == null)
+                    {
+                        try
+                        {
+                            GetNonceAndSetOrderContent();
+                        }
+                        catch (Exception ex)
+                        {
+                            LogError("Error: " + ex.Message);
+                            break;
+                        }
+                    }
+
+                    try
+                    {
+                        var tasks = Enumerable.Range(0, Data.Instance.SendCount).Select(i =>
+                        {
+                            return httpClient.PostAsync(sendOrderUrl, orderContent);
+                        });
+                        Log($"Sending {Data.Instance.SendCount} order...");
+                        var ordersStopwatch = Stopwatch.StartNew();
+                        Task.WhenAll(tasks).Wait();
+                        ordersStopwatch.Stop();
+                        Log($"{Data.Instance.SendCount} orders were sent in {ordersStopwatch.Elapsed.TotalSeconds} second");
+                    }
+                    catch (Exception ex)
+                    {
+                        LogError("Error: " + $"Error while sending orders: " + ex.Message);
+                    }
+
+                    if (DateTime.Now >= endDateTime)
+                    {
+                        break;
+                    }
+
+                    Log($"Waiting {Data.Instance.SendInterval} milliseconds...");
+                    Thread.Sleep(Data.Instance.SendInterval);
+                }
+                while (true);
+                Log($"Finish");
+            }));
+            orderThread.Priority = ThreadPriority.Highest;
+            var x = startDateTime.Subtract(DateTime.Now);
+            Log($"Start send orders at {startsAt.Hours}:{startsAt.Minutes}:{startsAt.Seconds}:{startsAt.Milliseconds}");
+            if (x.TotalMilliseconds > 0)
+            {
+                Log($"Waiting {x.TotalSeconds} Seconds...");
+                Thread.Sleep((int)x.TotalMilliseconds);
+            }
+
+            orderThread.Start();
+            Log("Order thread started");
+            nonceThread.Start();
+            Log("Nonce thread started");
+            Console.ReadKey();
+        }
+
+        private static void Log(string text)
+        {
+            Console.WriteLine($"{DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second}:{DateTime.Now.Millisecond}\t{text}");
+        }
+
+        private static void LogError(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"{DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second}:{DateTime.Now.Millisecond}\t{text}");
+            Console.ResetColor();
+        }
+
+        private static HttpClient GetHttpClient()
+        {
+            var socketsHandler = new SocketsHttpHandler
+            {
+                PooledConnectionLifetime = TimeSpan.FromMinutes(10),
+                PooledConnectionIdleTimeout = TimeSpan.FromMinutes(5),
+                MaxConnectionsPerServer = 10,
+                Proxy = Data.Instance.Proxy.Enabled ? new WebProxy(Data.Instance.Proxy.Value, false) : null,
+            };
+            HttpClient client = new HttpClient(socketsHandler);
+            client.DefaultRequestHeaders.Add("Host", "online.agah.com");
+            client.DefaultRequestHeaders.Add("X-Requested-With", "XMLHttpRequest");
+            client.DefaultRequestHeaders.Add(
+              "User-Agent",
+              "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36");
+            client.DefaultRequestHeaders.Add("Accept", "application/json, text/plain, */*");
+            client.DefaultRequestHeaders.Add("Origin", "https://online.agah.com");
+            client.DefaultRequestHeaders.Add("Sec-Fetch-Site", "same-origin");
+            client.DefaultRequestHeaders.Add("Sec-Fetch-Mode", "cors");
+            client.DefaultRequestHeaders.Add("Referer", "https://online.agah.com/");
+            client.DefaultRequestHeaders.Add("Cookie", Data.Instance.Cookie);
+            return client;
+        }
+    }
+}
